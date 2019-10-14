3472b4f10c5ab12c291476e87fb212fa8ee7cf8a
Antti Kangas <antti.kangas99@gmail.com>
2019-10-14 23:25:48 +0300

    Adds scrolling background
    This reverts commit ece2bdc21a16f266414692fd48fe6154f50c7069.

---
+++ Space Shooter/Assets/Scripts/BackgroundScroller.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Handles the texture scrolling of multilevel backgrounds
+public class BackgroundScroller : MonoBehaviour
+{
+    //List of all backgrounds from lower to higher
+    public List<MeshRenderer> backgrounds;
+
+    //How much more transparent every other layer is
+    [Range(0.1f, 0.9f)]
+    public float alphaFactor = 0.8f;
+
+    //How much slower every other layer moves
+    [Range(0, 10)]
+    public int parallaxFactor = 0;
+
+    void Update()
+    {
+        ScrollAll();
+    }
+
+    private void Awake()
+    {
+        SetAlphas();
+    }
+
+    //Scroll all textures depending on our parents position
+    void ScrollAll()
+    {
+        for (int i = 0; i < backgrounds.Count; i++)
+        {
+            Scroll(backgrounds[i].material, Mathf.FloorToInt(Mathf.Pow(2, backgrounds.Count - i + parallaxFactor)));
+        }
+    }
+
+    //Scroll one texture
+    void Scroll(Material mat, int parallax)
+    {
+        mat.mainTextureOffset = transform.position / parallax;
+    }
+
+    //Set background alphas and render positions
+    void SetAlphas()
+    {
+        for(int i = 0; i < backgrounds.Count; i++)
+        {
+            backgrounds[i].transform.position = new Vector3(transform.position.x, transform.position.y, backgrounds.Count - i);
+            Material mat = backgrounds[i].sharedMaterial;
+            mat.color = new Color(mat.color.r, mat.color.g, mat.color.b, Mathf.Pow(alphaFactor, i));
+        }
+    }
+
+    private void OnValidate()
+    {
+        SetAlphas();
+    }
+}
